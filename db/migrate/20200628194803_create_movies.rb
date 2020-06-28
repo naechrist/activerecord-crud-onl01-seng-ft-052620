38d8e53542c7c92463988647b75e8985402dc86b
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-28 19:53:58 +0000

    Automatically backed up by Learn

--- db/migrate/20200628194803_create_movies.rb
+++ db/migrate/20200628194803_create_movies.rb
@@ -1,5 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column 
+    add_column :movies, :title, :string 
+    add_column :movies, :release_date, :integer 
+    add_column :movies, :director, :string 
+    add_column :movies, 
   end
 end
